7adbcdf582385fc9e4ba99e0321076102af681db
meuira <mwardana611@gmail.com>
2026-09-19 07:13:31 +0000

    Revert "cpufreq: schedutil: Reset cached_raw_freq when not in sync with next_freq"
    This reverts commit 9ebfa403d5acd97decfeab33dbb02021b9430f70.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -343,8 +343,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		if (busy && next_f < sg_policy->next_freq &&
 		    sg_policy->next_freq != UINT_MAX) {
 			next_f = sg_policy->next_freq;
-
-			/* Reset cached freq as next_freq has changed */
+			/* clear cache when it's bypassed */
 			sg_policy->cached_raw_freq = 0;
 		}
 	}
